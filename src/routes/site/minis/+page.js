8be26752113c2196bb2c4ae419f0d7cc3c8e18d7
John Hochstetler <jg.hochstetler@gmail.com>
2022-09-16 23:52:29 -0700

    add trailing slash to api url

--- src/routes/site/minis/+page.js
+++ src/routes/site/minis/+page.js
@@ -2,7 +2,7 @@ import { variables } from '$lib/variables';
 
 /** @type {import('./$types').PageLoad} */
 export async function load(event) {
-    const url = `${variables.basePath}/minis`
+    const url = `${variables.basePath}/minis/`
     const res = await event.fetch(url ,{
         headers: {"Accept": "application/json"}
     });
