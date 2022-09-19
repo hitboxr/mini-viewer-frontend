acd1593f36be36ddcdb885e4cd8407c41d15de44
Forrest Deters <forrest.deters@gmail.com>
2022-09-19 07:55:53 -0700

    button padding changes

--- src/components/UI/Button.svelte
+++ src/components/UI/Button.svelte
@@ -1,18 +1,16 @@
 <style>
     button {
-        /* width: 100%; */
         min-width: 20rem;
         background-color: var(--color-primary);
         border: none;
         border-radius: 10rem;
         color: white;
-        padding: 1% 2%;
+        padding: 1rem 2rem;
         text-align: center;
         text-decoration: none;
         display: inline-block;
         font-size: inherit;
         font-weight: 700;
-        /* letter-spacing: 0.2rem; */
     }
 
     button:hover {
