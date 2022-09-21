c89716e224e08b754bfc63d49ff52c674dafc33b
Forrest Deters <forrest.deters@gmail.com>
2022-09-20 17:27:11 -0700

    translate mini size name to full

---
+++ src/lib/dictionaries.js
@@ -0,0 +1,10 @@
+export const sizeTranslator = {
+    "N": "None",
+    "T": "Tiny",
+    "S": "Small",
+    "M": "Medium",
+    "L": "Large",
+    "H": "Huge",
+    "G": "Gargantuan",
+    "C": "Colossal",
+}
--- src/routes/site/minis/[id]/+page.svelte
+++ src/routes/site/minis/[id]/+page.svelte
@@ -1,10 +1,13 @@
 <script>
+    import { sizeTranslator } from "$lib/dictionaries.js";
+
     /** @type {import('./$types').PageData} */
     export let data;
 
     const details = data.miniDetails;
 
     const altText = details.description ? details.description : details.name;
+    const fullSizeName = sizeTranslator[details.size];
 </script>
 
 <style>
@@ -18,8 +21,8 @@
     <title>{details.name}</title>
 </svelte:head>
 
-<img src="{details.mini_images[0]}" alt="altText">
+<img src="{details.mini_images[0]}" alt="{altText}">
 <p>Name: {details.name}</p>
 <p>Pack: {details.pack.pack_name}</p>
 <p>Description: {details.description}</p>
-<p>Size: {details.size}</p>
+<p>Size: {fullSizeName}</p>
