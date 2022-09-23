81569af4c2b6c627d871f2e827c99bfb5291eb2a
Forrest Deters <forrest.deters@gmail.com>
2022-09-22 19:33:07 -0700

    styling overhaul for homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <style>
-    header, main {
+    /* header, main {
         display: flex;
         flex-flow: column wrap;
         justify-content: center;
@@ -51,6 +51,75 @@
         color: inherit;
         text-decoration: none;
         text-transform: uppercase;
+    } */
+    .homepage {
+        min-height: 100vh;
+        background-color: var(--color-black-matte);
+        color: var(--color-gray-light);
+        text-align: center;
+    }
+
+    header {
+        padding: 0 2rem;
+        padding-top: 5vh;
+    }
+
+    h1 {
+        margin: 0 auto;
+        text-transform: uppercase;
+        font-size: 8rem;
+    }
+
+    h3 {
+        color: var(--color-gray);
+        text-transform: lowercase;
+        font-size: 2rem;
+    }
+
+    nav {
+        padding-top: 15vh;
+    }
+
+    ul {
+        padding-left: 0;
+        list-style: none;
+    }
+
+    li {
+        font-size: 4rem;
+        backface-visibility: hidden;
+    }
+
+    li:not(:last-child) {
+        margin-bottom: 10vh;
+    }
+
+    a {
+        color: inherit;
+        text-decoration: none;
+        text-transform: uppercase;
+
+        padding: .5rem 1rem;
+        background-image: linear-gradient(120deg, transparent 0%, transparent 50%, var(--color-gray-light) 50%);
+        background-size: 220%;
+        transition: all .4s;
+    }
+
+    a:hover,
+    a:focus {
+        color: var(--color-primary);
+        font-weight: bold;
+        background-position: 100%;
+    }
+
+    @media screen and (max-width: 480px) {
+        h1 {
+            font-size: 6rem;
+        }
+
+        li {
+            font-size: 3rem;
+        }
     }
 </style>
 
@@ -58,7 +127,7 @@
     <title>Mini City</title>
 </svelte:head>
 
-<header>
+<!-- <header>
     <h1>Mini City</h1>
     <p>A menagerie of 3D models for tabletop gameplay</p>
 </header>
@@ -68,4 +137,17 @@
         <li><a href="/about"><Button>About this site</Button></a></li>
         <li><Button>Fake link</Button></li>
     </ul>
-</main>
+</main> -->
+
+<div class="homepage">
+    <header>
+        <h1>Mini City</h1>
+        <h3>A menagerie of 3D models for tabletop gameplay</h3>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="/minis">Browse minis</a></li>
+            <li><a href="/about">About this site</a></li>
+        </ul>
+    </nav>
+</div>
